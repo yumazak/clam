f17c63356375e3a11e667cd37ce6131e161c7923
uma <blog3170@gmail.com>
2018-02-20 14:52:26 +0900

    change gitignore

--- src/template.rs
+++ src/template.rs
@@ -1,12 +1,20 @@
 extern crate regex;
 use self::regex::Regex;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+
 pub fn draw(html: &str) -> String {
     println!("{}", html);
     return html.to_string()
 }
-pub fn reg(str: &str, data: HashMap<&str, &str>) -> String {
+pub fn reg(str: &str, data: HashMap<&str, &str>, fname: &str) -> String {
     let re = Regex::new(r"<%[\s]*(.*?)[\s]*%>").unwrap();
+    match File::open(&args[2]) {
+            Ok(file) => find_string(BufReader::new(file), &args[1]),
+            Err(_) => println!("{} not found", &args[1])
+        }
     let mut result: String = "".to_string();
     for cap in re.captures_iter(str) {
         match data.get(&cap[1]) {
